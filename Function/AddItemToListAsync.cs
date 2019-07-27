59a50ec49beaf345e44ce9e73b7425f91fb945e8
ebonight337 <senndayuuki337@yahoo.co.jp>
2019-07-27 15:14:57 +0900

    Update AddItemToListAsync.cs

--- Function/AddItemToListAsync.cs
+++ Function/AddItemToListAsync.cs
@@ -1,113 +1,123 @@
- /// <summary>
-        /// 対象のリストにアイテムを非同期処理で追加します
-        /// </summary>
-        /// <param name="spList">追加対象のSharePointList</param>
-        /// <param name="item">追加するアイテム</param>
-        /// <param name="attachmentFilePaths">追加する添付ファイルパスリスト</param>
-        /// <param name="folderPath">追加する対象フォルダ（省略した場合はリストのルートフォルダ）</param>
-        /// <returns></returns>
-        public async Task<object> AddItemToListAsync(SP.List spList, IDictionary<string, object> item, IEnumerable<string> attachmentFilePaths, string folderPath = null)
+using System;
+using System.Net;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Threading;
+using System.Threading.Tasks;
+using SP = Microsoft.SharePoint.Client;
+
+/// <summary>
+/// 対象のリストにアイテムを非同期処理で追加します
+/// </summary>
+/// <param name="spList">追加対象のSharePointList</param>
+/// <param name="item">追加するアイテム</param>
+/// <param name="attachmentFilePaths">追加する添付ファイルパスリスト</param>
+/// <param name="folderPath">追加する対象フォルダ（省略した場合はリストのルートフォルダ）</param>
+/// <returns></returns>
+public async Task<object> AddItemToListAsync(SP.List spList, IDictionary<string, object> item, IEnumerable<string> attachmentFilePaths, string folderPath = null)
+{
+    jobLog.Write($"アイテム作成開始=> {item["Title"]}");
+    if (spList == null)
+    {
+        throw new ArgumentNullException(nameof(spList));
+    }
+    if (item == null)
+    {
+        throw new ArgumentNullException(nameof(item));
+    }
+
+    // フォルダの指定
+    var listItemCreationInfo = new SP.ListItemCreationInformation();
+    if (!string.IsNullOrEmpty(folderPath))
+    {
+        listItemCreationInfo.FolderUrl = folderPath;
+    }
+
+    var newItem = spList.AddItem(listItemCreationInfo);
+
+
+    // FileStream リソース管理用リスト
+    //  → ExecuteQueryAsync 呼び出し前に FileStream を using で解放してしまうと、
+    //      ExecuteQueryAsync の時点でファイル参照できなくなってしまうため、明示的にリソース管理
+    var fileStreamList = new List<FileStream>();
+
+    newItem["_ModerationStatus"] = 0;
+    newItem.UpdateOverwriteVersion();
+
+    try
+    {
+        if (attachmentFilePaths != null)
         {
-            jobLog.Write($"アイテム作成開始=> {item["Title"]}");
-            if (spList == null)
+            // 添付ファイルの追加処理
+            foreach (var filePath in attachmentFilePaths)
             {
-                throw new ArgumentNullException(nameof(spList));
-            }
-            if (item == null)
-            {
-                throw new ArgumentNullException(nameof(item));
-            }
-
-            // フォルダの指定
-            var listItemCreationInfo = new SP.ListItemCreationInformation();
-            if (!string.IsNullOrEmpty(folderPath))
-            {
-                listItemCreationInfo.FolderUrl = folderPath;
-            }
-
-            var newItem = spList.AddItem(listItemCreationInfo);
-
-
-            // FileStream リソース管理用リスト
-            //  → ExecuteQueryAsync 呼び出し前に FileStream を using で解放してしまうと、
-            //      ExecuteQueryAsync の時点でファイル参照できなくなってしまうため、明示的にリソース管理
-            var fileStreamList = new List<FileStream>();
-
-            newItem["_ModerationStatus"] = 0;
-            newItem.UpdateOverwriteVersion();
-
-            try
-            {
-                if (attachmentFilePaths != null)
+                if (!File.Exists(filePath))
                 {
-                    // 添付ファイルの追加処理
-                    foreach (var filePath in attachmentFilePaths)
-                    {
-                        if (!File.Exists(filePath))
-                        {
-                            newItem.DeleteObject();
-                            throw new NullReferenceException($"指定されたファイルが存在しません => {filePath}");
-                        }
-
-                        var fileStream = new FileStream(filePath, FileMode.Open);
-
-                        if (fileStream.Length == 0)
-                        {
-                            newItem.DeleteObject();
-                            throw new NullReferenceException($"ファイル容量が0です => {filePath}");
-                        }
-
-                        fileStreamList.Add(fileStream);
-                        var attachmentInfo = new SP.AttachmentCreationInformation
-                        {
-                            ContentStream = fileStream,
-                            FileName = Path.GetFileName(filePath)
-                        };
-
-                        newItem.AttachmentFiles.Add(attachmentInfo);
-                    }
-
+                    newItem.DeleteObject();
+                    throw new NullReferenceException($"指定されたファイルが存在しません => {filePath}");
                 }
 
-                // アイテム追加処理(添付ファイル前に実施すると、更新者が実行アカウントになってしまう)
-                foreach (var keyValue in item)
+                var fileStream = new FileStream(filePath, FileMode.Open);
+
+                if (fileStream.Length == 0)
                 {
-                    // 作成日時がない場合は現在の日時挿入
-                    if ((keyValue.Key.ToString() == "Created") && ((keyValue.Value.Equals(""))))
-                    {
-                        newItem[keyValue.Key] = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
-                    }
-                    else
-                    {
-                        newItem[keyValue.Key] = keyValue.Value;
-                    }
-                    // ColumNameとしてCreatedを入れているのにnullにすることはできない
-                    // if ((keyValue.Key.ToString() == "Created") && ((keyValue.Value.Equals(""))))
-                    // {
-                    //     newItem.DeleteObject();
-                    //     throw new Exception("アイテムの作成日時プロパティが不足しています");
-                    // }
+                    newItem.DeleteObject();
+                    throw new NullReferenceException($"ファイル容量が0です => {filePath}");
                 }
 
-                newItem.Update();
-                // サーバーにリクエスト(429、503のエラーが発生するとリトライを行う)
-                await ExecuteQuery.ExecuteQueryWithIncrementalRetry(_context);
-                jobLog.Write($"アイテム作成完了=> {item["Title"]}");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"アイテム作成中にエラーが発生しました 詳細ログを確認してください");
-                jobLog.Write("アイテム作成中に以下のエラーが発生しました\r\n" + e);
-                return null;
-            }
-            finally
-            {
-                // 添付ファイルの後処理
-                foreach (var fileStream in fileStreamList)
+                fileStreamList.Add(fileStream);
+                var attachmentInfo = new SP.AttachmentCreationInformation
                 {
-                    fileStream.Dispose();
-                }
+                    ContentStream = fileStream,
+                    FileName = Path.GetFileName(filePath)
+                };
+
+                newItem.AttachmentFiles.Add(attachmentInfo);
             }
 
-            return newItem;
         }
+
+        // アイテム追加処理(添付ファイル前に実施すると、更新者が実行アカウントになってしまう)
+        foreach (var keyValue in item)
+        {
+            // 作成日時がない場合は現在の日時挿入
+            if ((keyValue.Key.ToString() == "Created") && ((keyValue.Value.Equals(""))))
+            {
+                newItem[keyValue.Key] = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+            }
+            else
+            {
+                newItem[keyValue.Key] = keyValue.Value;
+            }
+            // ColumNameとしてCreatedを入れているのにnullにすることはできない
+            // if ((keyValue.Key.ToString() == "Created") && ((keyValue.Value.Equals(""))))
+            // {
+            //     newItem.DeleteObject();
+            //     throw new Exception("アイテムの作成日時プロパティが不足しています");
+            // }
+        }
+
+        newItem.Update();
+        // サーバーにリクエスト(429、503のエラーが発生するとリトライを行う)
+        await ExecuteQuery.ExecuteQueryWithIncrementalRetry(_context);
+        jobLog.Write($"アイテム作成完了=> {item["Title"]}");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"アイテム作成中にエラーが発生しました 詳細ログを確認してください");
+        jobLog.Write("アイテム作成中に以下のエラーが発生しました\r\n" + e);
+        return null;
+    }
+    finally
+    {
+        // 添付ファイルの後処理
+        foreach (var fileStream in fileStreamList)
+        {
+            fileStream.Dispose();
+        }
+    }
+
+    return newItem;
+}
